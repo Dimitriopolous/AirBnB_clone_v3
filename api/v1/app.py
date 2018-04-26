3845e2d17c27141ba538952bb4731055787dd0d4
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-26 22:55:31 +0000

    Installed and integrated CORS into API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+*#!/usr/bin/python3
 '''
     This Python mode contains the custom-built API for the HBnB project
 '''
@@ -7,10 +7,11 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 @app.teardown_appcontext
 def teardownContext(exception):
