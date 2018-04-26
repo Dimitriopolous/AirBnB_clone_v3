abf53e0fe5d59f03f5d252156580a6f5548612f7
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-26 01:31:55 +0000

    Created an api file that handles GET requests. POST, PUT, and DELETE have bugs that need fixing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+'''
+    Module for counting the number of objects in each class
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import classes, storage
+
 
 @app_views.route('/status', strict_slashes=False)
 def returnStatus():
@@ -11,7 +16,14 @@ def returnStatus():
 def retrieveNumbers():
     ''' retrieves the number of each object by type '''
     counting_dict = {}
-    for key, value in classes.items():
-        if key != "BaseModel":
-            counting_dict[key] = storage.count(key)
+    class_dict = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    for key, value in class_dict.items():
+        counting_dict[value] = storage.count(key)
     return jsonify(counting_dict)
---
+++ api/v1/views/states.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+'''
+    Blueprint app_views decorators
+    for HTTP requests that allow users
+    to manipulate data in storage
+'''
+
+from api.v1.views import app_views
+from flask import jsonify
+from flask import abort
+from flask import request
+from models import classes, storage
+from models.state import State
+import json
+
+
+@app_views.route('/states', strict_slashes=False, methods=['GET'])
+def get_all_states():
+    ''' Gets a dictionary of all states then jsonifies and returns it '''
+    states = []
+    all_states = storage.all("State")
+    for key, val in all_states.items():
+        states.append(val.to_dict())
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+def get_one_state(state_id=None):
+    state = jsonify(storage.get("State", state_id).to_dict())
+    if state == None:
+        abort(404)
+    return state
+
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def post_new_state():
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    else:
+        if 'name' in data:
+            return jsonify(State().to_dict(), 201)
+        else:
+            abort(400, 'Missing name')
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['DELETE'])
+def delete_state(state_id=None):
+    obj = storage.get("State", state_id)
+    if obj is None:
+        abort(404)
+    else:
+        storage.delete(obj)
+        check = jsonify(storage.get("State", state_id).to_dict())       #Verifying if object was successfully deleted
+        return check, 200                                               #If doesn't work we can just return {}
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def put_to_state(state_id=None):
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    obj = storage.get("State", state_id)
+    if obj is None:
+        abort(404)
+    else:
+        for key, val in data.items():
+            if key in obj and key not in ['id', 'created_at', 'updated_at']:
+                setattr(obj, key, val)
+        obj.save()
+        return jsonify(obj.to_dict())
+
+
+
+
+
+
+    
+
+
+#    @app_views.route('/states/<state_id>', strict_slashes=False,
+#                     methods=['GET', 'DELETE', 'PUT'])
+#    def httpStates(state_id=None):
+#        ''' GETs, POSTs, DELETEs, or PUTs to all State objects '''
+#        print("Got into httpStates() method")
+#        if request.method == 'GET':
+#            print("GET method")
+#            if state_id is None or state_id == '':
+#                print("No state_id passed")
+#                states = []
+#                all_states = storage.all("States").values()
+#                for item in all_states:
+#                    print("Iterating through all_states")
+#                    states.append(item.to_dict())
+#                return jsonify(states)
+#            else:
+#                print("Received state_id")
+#                state = jsonify(storage.get("State", state_id).to_dict())
+#                if state == None:
+#                    abort(404)
+#                return state
+#    
+#        elif request.method == 'DELETE':
+#            obj = storage.get("State", state_id)
+#            if obj is None:
+#                abort(404)
+#            else:
+#                storage.delete(obj)
+#                check = jsonify(storage.get("State", state_id).to_dict())       #Verifying if object was successfully deleted
+#                return check, 200                                               #If doesn't work we can just return {}
+#    
+#        elif request.method == 'POST':
+#            data = request.get_json()
+#            if data is None:
+#                abort(400, 'Not a JSON')
+#            else:
+#                if 'name' in data:
+#                    return jsonify(State().to_dict(), 201)
+#                else:
+#                    abort(400, 'Missing name')
+#    
+#        elif request.method == 'PUT':
+#            data = request.get_json()
+#            if data is None:
+#                abort(400, 'Not a JSON')
+#            obj = storage.get("State", state_id)
+#            if obj is None:
+#                abort(404)
+#            else:
+#                for key, val in data.items():
+#                    if key in obj and key not in ['id', 'created_at', 'updated_at']:
+#                        setattr(obj, key, val)
+#                obj.save()
+#                    return jsonify(obj.to_dict())
