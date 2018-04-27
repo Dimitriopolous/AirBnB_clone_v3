3fbfa19da39b39c7a5f2a04bc74b1d007376c6ce
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-27 00:26:24 +0000

    Receive host and port from input

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,4 +26,5 @@ def page_not_found(e):
     return not_found, 404
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=os.getenv('HBNB_API_HOST', default='0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', default=5000)))
